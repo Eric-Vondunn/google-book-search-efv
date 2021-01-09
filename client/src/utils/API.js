25bec71bfcc95345068000217022bf04ee2b40a2
Eric Vondunn <ericvondunn40@gmail.com>
2021-01-09 09:54:26 -0500

    pages and utils folder

---
+++ client/src/utils/API.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export default {
+  //books from google api
+  getBooks: function (q) {
+    return axios.get("/api/google", { params: { q: "title:" + q } });
+  },
+
+  //get saved books
+  getSavedBooks: function () {
+    return axios.get("/api/books");
+  },
+  //delete svd books by id
+  deleteBook: function (id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // save book to db
+  saveBook: function (bookData) {
+    return axios.post("/api/books", bookData);
+  },
+};
